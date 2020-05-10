bc0262baa997d9169701e29691a192fd1f4236ff
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-10 05:22:17 +0300

    Progress

--- src/conf/plugins/input/http/RECON_REPORT.groovy
+++ src/conf/plugins/input/http/RECON_REPORT.groovy
@@ -2,12 +2,20 @@ package conf.plugins.input.http
 
 import groovy.sql.Sql
 import io.infinite.blackbox.BlackBox
+import io.infinite.pigeon.springdatarest.repositories.InputMessageRepository
 import io.infinite.supplies.ast.exceptions.ExceptionUtils
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext
 import org.springframework.stereotype.Component
+import org.springframework.web.context.ContextLoader
+import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.context.support.SpringBeanAutowiringSupport
+import org.springframework.web.context.support.WebApplicationContextUtils
 
+import javax.servlet.ServletContext
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.sql.DataSource
@@ -19,9 +27,11 @@ class ReconReport {
     @Autowired
     DataSource dataSource
 
-    String run() {
+    String run(ServletContext servletContext) {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this)
         Sql sql = new Sql(dataSource)
+        WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext)
+        applicationContext.autowireCapableBeanFactory.autowireBean(this)
         return sql.firstRow("select * from messages").toString()
     }
 }
@@ -30,6 +40,7 @@ class ReconReport {
 def applyPlugin() {
     HttpServletRequest httpServletRequest = binding.getVariable("httpServletRequest") as HttpServletRequest
     HttpServletResponse httpServletResponse = binding.getVariable("httpServletResponse") as HttpServletResponse
+    InputMessageRepository inputMessageRepository = binding.getVariable("inputMessageRepository") as InputMessageRepository
     String requestBody = binding.getVariable("requestBody")
     def log = LoggerFactory.getLogger(this.getClass())
     try {
@@ -40,7 +51,7 @@ def applyPlugin() {
         for (headerName in httpServletRequest.getHeaderNames()) {
             log.info(headerName + ":" + httpServletRequest.getHeader(headerName))
         }
-        return new ReconReport().run()
+        return new ReconReport().run(httpServletRequest.servletContext)
     } catch (Exception e) {
         log.error(e.getMessage(), e)
         log.info(httpServletRequest.getRequestURI())
