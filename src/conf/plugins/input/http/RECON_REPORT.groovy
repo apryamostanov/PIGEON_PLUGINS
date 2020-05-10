2bf9a45de5cb204893cd3853a3ea6b6f28ad6cfe
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-10 05:53:03 +0300

    Progress

--- src/conf/plugins/input/http/RECON_REPORT.groovy
+++ src/conf/plugins/input/http/RECON_REPORT.groovy
@@ -2,13 +2,20 @@ package conf.plugins.input.http
 
 import groovy.sql.Sql
 import io.infinite.blackbox.BlackBox
+import io.infinite.pigeon.springdatarest.entities.InputMessage
 import io.infinite.pigeon.springdatarest.repositories.InputMessageRepository
 import io.infinite.supplies.ast.exceptions.ExceptionUtils
+import org.apache.commons.lang3.time.FastDateFormat
+import org.apache.commons.lang3.time.FastDateParser
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.stereotype.Component
 import org.springframework.web.context.ContextLoader
 import org.springframework.web.context.WebApplicationContext
@@ -22,17 +29,39 @@ import javax.sql.DataSource
 
 def log = LoggerFactory.getLogger(this.getClass())
 
+
+@RepositoryRestResource
+interface CustomInputMessageRepository extends JpaRepository<InputMessage, Long> {
+
+    @Query("""select i from InputMessage i
+        join i.outputMessages o
+        join o.httpLogs h
+        where i.inputQueueName = :inputQueueName
+        and i.insertTime between :dateFrom and :dateTo""")
+    Set<InputMessage> findByDates(
+            @Param("inputQueueName") String inputQueueName,
+            @Param("dateFrom") Date dateFrom,
+            @Param("dateTo") Date dateTo
+    )
+
+}
+
 @Component
 class ReconReport {
-    @Autowired
-    DataSource dataSource
 
-    String run(ServletContext servletContext) {
+    @Autowired
+    CustomInputMessageRepository customInputMessageRepository
+
+    String run(ServletContext servletContext, String inputQueueName, Date dateFrom, Date dateTo) {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this)
         WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext)
         applicationContext.autowireCapableBeanFactory.autowireBean(this)
-        Sql sql = new Sql(dataSource)
-        return sql.firstRow("select * from messages").toString()
+
+        return customInputMessageRepository.findByDates(
+                inputQueueName,
+                dateFrom,
+                dateTo
+        )
     }
 }
 
@@ -51,7 +80,13 @@ def applyPlugin() {
         for (headerName in httpServletRequest.getHeaderNames()) {
             log.info(headerName + ":" + httpServletRequest.getHeader(headerName))
         }
-        return new ReconReport().run(httpServletRequest.servletContext)
+        FastDateFormat fastDateFormat = FastDateFormat.getInstance("yyyyMMdd")
+        return new ReconReport().run(
+                httpServletRequest.servletContext,
+                httpServletRequest.getParameter("inputQueueName"),
+                fastDateFormat.parse(httpServletRequest.getParameter("dateFrom")),
+                fastDateFormat.parse(httpServletRequest.getParameter("dateTo"))
+        )
     } catch (Exception e) {
         log.error(e.getMessage(), e)
         log.info(httpServletRequest.getRequestURI())
